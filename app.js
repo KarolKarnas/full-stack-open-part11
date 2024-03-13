351f96a9f1db4db3308bff3e336bff89e7d2419e
KarolKarnas <karol.karnas@gmail.com>
2024-03-13 12:56:23 +0100

    create simple express server
    - use the server in the pipeline

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const jsonServer = require('json-server')
+const app = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// get the port from env variable
+const PORT = process.env.PORT || 5000
+
+
+app.use('/api', middlewares, router)
+app.use(express.static('build'))
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  // if (true) throw('error...  ')
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -4,9 +4,4 @@ describe('Anecdote App', function() {
     cy.visit('http://localhost:3000')
     cy.contains('Anecdotes')
   })
-  it('renders anecdotes', function() {
-    cy.visit('http://localhost:3000')
-    cy.contains('Any fool can write code that a computer can understand. Good programmers write code that humans can understand')
-    cy.contains('Adding manpower to a late software project makes it later!')
-  })
 })
